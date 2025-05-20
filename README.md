b4778371a50e999c7aade84ef09d08f6334a4c5a
KhazAkar <damianzrb@zohomail.eu>
2025-05-20 22:13:38 +0200

    docs: enhance readme

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# autonomous_gardening_system
+# Autonomous Gardening System
 
 Full stack ( LaFrite FE + BE and web FE+BE) implementation of autonomous gardening system, which will help my bell peppers grow 🌱
+
+## 🤝 Contributing - Device
+
+0. Install uv
+
+1. Clone the repo
+
+```bash
+git clone https://codeberg.org/KhazAkar/autonomous_gardening_system
+cd autonomous_gardening_system/device
+```
+
+2. Build the project
+
+```bash
+uv sync --all-extras
+docker build -t local_device
+```
+
+4. Run the tests
+
+```bash
+uv pytest tests/
+```
+
+If you'd like to contribute, please fork the repository and open a pull request to the `master` branch.
--- device/main.py
+++ device/main.py
@@ -1,11 +1,20 @@
 from falcon.asgi import App
+from falcon import Response
 import falcon
 
+
 class HelloWorld:
-    async def on_get(self, req, resp):
-        resp.media = {'message': 'Hello, world!'}
-        resp.content_type = 'application/json'
+    async def on_get(self, req, resp: Response):
+        resp.media = {"message": "Hello, world!"}
+        resp.content_type = "application/json"
         resp.status = falcon.HTTP_200
 
+    async def on_post(self, req, resp: Response):
+        resp.media = {"message": "Hello, world!"}
+        resp.content_type = "application/json"
+        resp.status = falcon.HTTP_200
+
+
 app = App()
-app.add_route('/', HelloWorld())
+app.add_route("/", HelloWorld())
+
